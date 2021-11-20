881b88d3d9ad3dd22daae9cfa7329d5faa2fc953
Ishmael <ishmaelharris3@gmail.com>
2021-11-19 17:35:53 -0800

    finished sections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,28 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:">510.abc.1234</LinkItem>
+        </LinkColumn>
+
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:ish@ishmaelharris.dev">ish@ishmaelharris.dev</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      
+      <SocialContainer>
+      <SocialIcons href="https://github.com/IshmaelHarris">
+        <AiFillGithub size = "3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://linkedin.com/in/ishmaelharris/">
+        <AiFillLinkedin size = "3rem"/>
+      </SocialIcons>
+      </SocialContainer>
+    </FooterWrapper>
   );
 };
 
